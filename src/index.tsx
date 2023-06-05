30e3d21fd554f01db61cb8a81ce2b8a658080620
Kiri1111 <92_medved@mail.ru>
2023-06-05 22:27:12 +0300

    feat: router

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>Hello, its main page</div>
+	},
+	{
+		path: '/login',
+		element: <div>Hello, its login page</div>
+	}, {
+		path: '/logout',
+		element: <div>Hello, its logout page</div>
+	}, {
+		path: '/register',
+		element: <div>Hello, its register page</div>
+	}, {
+		path: '/profile',
+		element: <div>Hello, its profile page</div>
+	}
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}/>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
