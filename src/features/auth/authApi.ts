5b3f64e7494777f346093bf3993822df3ee53898
Kiri1111 <92_medved@mail.ru>
2023-06-13 13:01:18 +0300

    feat: auth feature

--- src/features/auth/authApi.ts
+++ src/features/auth/authApi.ts
@@ -5,32 +5,22 @@ export const authApi = {
 		return instance.post<RegisterResponseType>('auth/register', arg)
 	},
 	login(arg: ArgLoginType) {
-		return instance.post('auth/login', arg)
+		return instance.post<ProfileType>('auth/login', arg)
 	}
 }
 
-// export type ResponseType<D = {}> = {
-// 	resultCode: number
-// 	messages: Array<string>
-// 	fieldsErrors?: Array<string>
-// 	data: D
-// }
-
-export type ArgRegisterType = {
-	email: string,
-	password: string
-
-}
 export type ArgLoginType = {
 	email: string,
 	password: string
 	rememberMe: boolean
 }
-
+export type ArgRegisterType = Omit<ArgLoginType, 'rememberMe'>
+//export type _ArgRegisterType = Pick<ArgLoginType, 'password' | 'email'>
 export type RegisterResponseType = {
 	addedUser: UserType
 }
-export type UserType = {
+export type UserType = Omit<ProfileType, 'token' | 'tokenDeathTime'>
+export type ProfileType = {
 	_id: string
 	email: string
 	rememberMe: boolean
@@ -41,4 +31,6 @@ export type UserType = {
 	created: string
 	updated: string
 	_v: number
+	token: string
+	tokenDeathTime: number
 }
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,20 +1,30 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {ArgLoginType, ArgRegisterType, authApi} from "./authApi";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {ArgLoginType, ArgRegisterType, authApi, ProfileType} from "./authApi";
 
 const register = createAsyncThunk('auth/register', (arg: ArgRegisterType, thunkAPI) => {
 	const {dispatch, getState, rejectWithValue} = thunkAPI
-	authApi.register(arg).then((res) => console.log(res))
+	authApi.register(arg).then((res) => {
+		console.log(res)
+	})
 })
 
 const login = createAsyncThunk('auth/login', (arg: ArgLoginType, thunkAPI) => {
-	authApi.login(arg).then((res) => console.log(res))
+	const {dispatch, getState, rejectWithValue} = thunkAPI
+	authApi.login(arg).then((res) => dispatch(authActions.setProfile({profile: res.data})))
 })
 
 const slice = createSlice({
 	name: 'auth',
-	initialState: {},
-	reducers: {}
+	initialState: {
+		profile: null as ProfileType | null
+	},
+	reducers: {
+		setProfile(state, action: PayloadAction<{ profile: ProfileType }>) {
+			state.profile = action.payload.profile
+		}
+	}
 })
 
 export const authReducer = slice.reducer
+export const authActions = slice.actions
 export const authThunks = {register, login}
