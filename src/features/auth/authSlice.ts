7c54bca9aad26672cc5ba261ec88a0dac06ebb9a
Kiri1111 <92_medved@mail.ru>
2023-06-14 09:47:30 +0300

    feat: extra reducers

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,30 +1,42 @@
 import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {ArgLoginType, ArgRegisterType, authApi, ProfileType} from "./authApi";
 
-const register = createAsyncThunk('auth/register', (arg: ArgRegisterType, thunkAPI) => {
-	const {dispatch, getState, rejectWithValue} = thunkAPI
-	authApi.register(arg).then((res) => {
-		console.log(res)
-	})
+const register = createAsyncThunk('auth/register', async (arg: ArgRegisterType) => {
+	const res = await authApi.register(arg)
 })
 
-const login = createAsyncThunk('auth/login', (arg: ArgLoginType, thunkAPI) => {
-	const {dispatch, getState, rejectWithValue} = thunkAPI
-	authApi.login(arg).then((res) => dispatch(authActions.setProfile({profile: res.data})))
+// const login = createAsyncThunk('auth/login',  (arg: ArgLoginType, thunkAPI) => {
+// 	const {dispatch, getState, rejectWithValue} = thunkAPI
+// 	return authApi.login(arg).then((res) => {
+// 		return {profile: res.data}
+// 	})
+// })
+const login = createAsyncThunk('auth/login', async (arg: ArgLoginType) => {
+	const res = await authApi.login(arg)
+	return {profile: res.data}
 })
-
 const slice = createSlice({
 	name: 'auth',
 	initialState: {
 		profile: null as ProfileType | null
 	},
 	reducers: {
-		setProfile(state, action: PayloadAction<{ profile: ProfileType }>) {
-			state.profile = action.payload.profile
-		}
+		// setProfile(state, action: PayloadAction<{ profile: ProfileType }>) {
+		// 	state.profile = action.payload.profile
+		// },
+
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(login.fulfilled, (state, action) => {
+				state.profile = action.payload.profile
+			})
+			.addCase(register.rejected, (state, action) => {
+				console.log('warning, some mistake')
+			})
 	}
 })
 
 export const authReducer = slice.reducer
-export const authActions = slice.actions
+// export const authActions = slice.actions
 export const authThunks = {register, login}
